58f4e04b1619c3e4191e99739cd005d04f8346bf
1XTR.homepc <1XTR.homepc@trendco.space>
2020-03-20 01:07:55 +0300

    Changes

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 'use strict';
 window.addEventListener('load', function () {
     let products = new Product(6);
-    let but = products.buyButtons();
-    let buy = products.addProdToCart;
-    products.addListner(but, buy);
+
 });
 
 class Product {
     constructor(quantity) {
         this.container = document.querySelector('.product');
+        this.basketTable = document.querySelector('.basket__table');
+        this.basketEmpty = document.querySelector('.basket__empty');
+        this.buyButtons = document.querySelectorAll('.addToCart');
+        this.delButtons = document.querySelectorAll('.basket__delete');
+
         for (let i=1; i <= quantity; i++) {
             let prodName = 'Product ' + i;
             let price = 100 * i;
@@ -29,21 +32,24 @@ class Product {
             let prodPrice = document.createElement('p');
             prodPrice.classList.add('product__price');
             prodPrice.setAttribute('data-price', price);
-            prodPrice.innerText = price + " руб.";
+            prodPrice.innerText = "$" + price;
             prodDesc.append(prodPrice);
             newProd.append(prodDesc);
             let button = document.createElement('button');
             button.classList.add('addToCart');
             button.innerText = 'Купить';
+            button.addEventListener('click', this.addProdToCart);
             newProd.append(button);
             this.container.append(newProd);
         }
-    }
-    buyButtons() {
-        return document.querySelectorAll('.addToCart');
+        //this.addListener(this.buyButtons, this.addProdToCart);
+        this.addListener(this.delButtons, this.actionInBasket.bind(this));
     }
 
-    addListner(el, func) {
+
+
+
+    addListener(el, func) {
         if (typeof el.length == 'undefined') {
             el.addEventListener('click', func);
         } else if (el.length > 0 ) {
@@ -52,7 +58,34 @@ class Product {
     }
 
     addProdToCart() {
-        console.log(new Date());
+        console.log(event.target);
+    }
+    delProdFromCart() {
+        let removeObj = event.target.parentNode.parentNode;
+        removeObj.remove();
+    }
+
+    actionInBasket() {
+        this.delProdFromCart();
+        if (this.isBasketEmpty()) {
+            this.hideItem(this.basketTable);
+
+        }
+    }
+
+    hideItem(obj) {
+        obj.classList.add('hidden');
+    }
+
+    unhideItem(obj) {
+        obj.classList.remove('hidden');
+    }
+
+    isBasketEmpty() {
+        let div = this.basketTable;
+        if (div.children.length === 1) {
+            return true;
+        }
     }
 }
 
